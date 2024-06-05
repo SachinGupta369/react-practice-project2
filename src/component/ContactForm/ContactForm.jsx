117731c6e00af96731842cba5d012ad08cc3a5dc
Sachin Gupta <sachingupta0098@gmail.com>
2024-06-05 23:49:57 +0530

    done

--- src/component/ContactForm/ContactForm.jsx
+++ src/component/ContactForm/ContactForm.jsx
@@ -1,10 +1,27 @@
-import { MdMessage } from "react-icons/md";
+import { MdMessage, MdPadding } from "react-icons/md";
 import { FaPhone } from "react-icons/fa6";
 import { IoIosMail } from "react-icons/io";
 import Button from "../Button/Button";
 import styles from "./ContactForm.module.css";
+import { useState } from "react";
 
 const ContactForm = () => {
+  let [name, namee] = useState("");
+  let [email, mail] = useState("");
+  let [comment, area] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    namee(event.target[0].value);
+    mail(event.target[1].value);
+    area(event.target[2].value);
+
+    // console.log("Name",event.target[0].value);
+    //   console.log("Email",event.target[1].value);
+    //   console.log("Comment",event.target[2].value);
+  };
+
   return (
     <section className={styles.container}>
       <div className={styles.Contactform}>
@@ -20,22 +37,24 @@ const ContactForm = () => {
           text="VIA EMAIL FORM"
           icon={<IoIosMail fontSize="24px" />}
         />
-        <form>
+        <form onSubmit={onSubmit}>
           <div className={styles.form_control}>
             <label htmlFor="name">Name</label>
-            <input type="text" name="name"/>
+            <input type="text" name="name" />
           </div>
           <div className={styles.form_control}>
             <label htmlFor="mail">E-mail</label>
-            <input type="email" name="mail"/>
+            <input type="email" name="mail" />
           </div>
           <div className={styles.form_control}>
             <label htmlFor="text">Text</label>
-            <textarea name="text" rows="8"/>
+            <textarea name="text" rows="8" />
           </div>
-          <div style={
-            {display:"flex",justifyContent:"end" }}>
-            <Button text="SUBMIT"/>
+          <div style={{ display: "flex", justifyContent: "end" }}>
+            <Button text="SUBMIT" />
+          </div>
+          <div style={{ display: " " }}>
+            {name + " " + email + " " + comment}
           </div>
         </form>
       </div>
